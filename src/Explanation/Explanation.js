2652c9f6576d3f2ddf874dad883149b8d136d59d
Sarah Drew <sarahdrew93@gmail.com>
2020-01-13 16:34:57 -0500

    added basic form to explanation

--- src/Explanation/Explanation.js
+++ src/Explanation/Explanation.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 
 export default class Explanation extends Component {
     render() {
+        constructor(props) {
+            super(props);
+            this.state = {
+                kitchen: ''
+
+            }
+        }
         return (
             <>
                 <div className="explanation">
+                    <h1>INSPIRE ME TO COOK</h1>
                     <p>You've just realized you will have to make plans for your own dinner every day for the rest of your life. You have a few things in your kitchen and you're headed to the grocery store. But you need inspiration for what to make. </p>
                     <p>Enter the items you have right now (maybe garlic, and bell peppers, and eggs?) and hit submit to get inspiration for what else you need from the grocery store for a delicious meal.</p>
+                </div>
+                <div class="food-form">
+                    <form class="food">
+                        What's in my kitchen:<input type="text" placeholder="garlic, bell peppers, eggs"></input>
+                        <button type="submit">submit</button>
+                    </form>
                 </div>
             </>
         )
---
+++ src/Results/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Results extends Component {
+    render() {
+        return (
+            <>
+                <p>Here's what you should make:</p>
+            </>
+        )
+    }
+
+}
+
+//http://www.recipepuppy.com/about/api/
